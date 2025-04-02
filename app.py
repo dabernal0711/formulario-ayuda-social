7c7bff491250d75a85edf5387aba01594abd086d
dabernal0711 <69758523+dabernal0711@users.noreply.github.com>
2025-04-02 03:24:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,30 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta http-equiv="Content-Style-Type" content="text/css">
-  <title></title>
-  <meta name="Generator" content="Cocoa HTML Writer">
-  <meta name="CocoaVersion" content="2575.4">
-  </style>
-</head>
-<body>
-<p class="p1">from flask import Flask, render_template, request, send_file</p>
-<p class="p1">from docx import Document</p>
-<p class="p1">import uuid</p>
-<p class="p1">import os</p>
-<p class="p2"><br></p>
-<p class="p1">app = Flask(__name__)</p>
-<p class="p2"><br></p>
-<p class="p1">@app.route('/')</p>
-<p class="p1">def formulario():</p>
-<p class="p1"><span class="Apple-converted-space">    </span>return render_template('formulario.html')</p>
-<p class="p2"><br></p>
-<p class="p1">@app.route('/enviar', methods=['POST'])</p>
-<p class="p1">def enviar():</p>
-<p class="p1"><span class="Apple-converted-space">    </span>nombre = request.form['nombre']</p>
-<p class="p1"><span class="Apple-converted-space">    </span>cedula = request.form['cedula']</p>
-<p class="p1"><span class="Apple-converted-space">    </span>motivo = request.form['motivo']</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">    </span>doc = Document('plantilla.docx')</p>
-<p class="p1"><span class="Apple-converted-space">    </span>for p in doc.paragraphs:</p>
-<p class="p1"><span class="Apple-converted-space">        </span>p.text = p.text.replace('{{nombre}}', nombre)</p>
-<p class="p1"><span class="Apple-converted-space">        </span>p.text = p.text.replace('{{cedula}}', cedula)</p>
-<p class="p1"><span class="Apple-converted-space">        </span>p.text = p.text.replace('{{motivo}}', motivo)</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">    </span>filename = f'documento_{uuid.uuid4().hex}.docx'</p>
-<p class="p1"><span class="Apple-converted-space">    </span>doc.save(filename)</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">    </span>return send_file(filename, as_attachment=True)</p>
-<p class="p2"><br></p>
-<p class="p1">if __name__ == '__main__':</p>
-<p class="p1"><span class="Apple-converted-space">    </span>app.run()</p>
-</body>
-</html>
+from flask import Flask, render_template, request, send_file
+from docx import Document
+import uuid
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def formulario():
+    return render_template('formulario.html')
+
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    nombre = request.form['nombre']
+    cedula = request.form['cedula']
+    motivo = request.form['motivo']
+
+    doc = Document('plantilla.docx')
+    for p in doc.paragraphs:
+        p.text = p.text.replace('{{nombre}}', nombre)
+        p.text = p.text.replace('{{cedula}}', cedula)
+        p.text = p.text.replace('{{motivo}}', motivo)
+
+    filename = f'documento_{uuid.uuid4().hex}.docx'
+    doc.save(filename)
+
+    return send_file(filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run()
